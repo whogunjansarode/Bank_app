f8e6d124a318d405c6d05d8ab73db6fd01116e10
whogunjansarode <sgunjii989@gmail.com>
2023-05-26 11:07:22 +0530

    initial commit

---
+++ bank_app/src/components/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default CTA
