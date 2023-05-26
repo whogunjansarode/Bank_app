f540c9786b98ab27190e9182b63c4b37fa70cd44
whogunjansarode <sgunjii989@gmail.com>
2023-05-26 11:12:45 +0530

    checking if working

--- bank_app/src/components/Navbar.jsx
+++ bank_app/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {navLinks} from '../constants';
 const Navbar = () => {
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      
+      <h1>Okay try if it os working or notttt i have done dont know what im seeing</h1>
     </nav>
   )
 }
